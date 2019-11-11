36c3ddc3ad9700eb26e66aefe3cb894cd7bf8e51
Kubit, Magda <magdalena.kubit@pega.com>
2019-11-11 20:40:26 +0100

    Add quickSort package

---
+++ quickSort/quickSort.go
@@ -0,0 +1,29 @@
+package quickSort
+
+import "math/rand"
+
+func quickSort(unsortedTable []int32) ([]int32, error) {
+	if len(unsortedTable) < 2 {
+		return unsortedTable, nil
+	}
+
+	left, right := 0, len(unsortedTable)-1
+
+	pivot := rand.Int() % len(unsortedTable)
+
+	unsortedTable[pivot], unsortedTable[right] = unsortedTable[right], unsortedTable[pivot]
+
+	for i, _ := range unsortedTable {
+		if unsortedTable[i] < unsortedTable[right] {
+			unsortedTable[left], unsortedTable[i] = unsortedTable[i], unsortedTable[left]
+			left++
+		}
+	}
+
+	unsortedTable[left], unsortedTable[right] = unsortedTable[right], unsortedTable[left]
+
+	_, _ = quickSort(unsortedTable[:left])
+	_, _ = quickSort(unsortedTable[left+1:])
+
+	return unsortedTable, nil
+}
